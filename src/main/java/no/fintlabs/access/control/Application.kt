71bc03dae585fd8f7be3afc5aa9c43b8f3ea1c30
Trond Magnus Sevre <trond@fintlabs.no>
2025-01-31 09:31:10 +0100

    build: Change application and build to make the spring application run

--- src/main/java/no/fintlabs/access/control/Application.kt
+++ src/main/java/no/fintlabs/access/control/Application.kt
@@ -1,12 +1,12 @@
 package no.fintlabs.access.control
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-object Application {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        SpringApplication.run(Application::class.java, *args)
-    }
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
+
